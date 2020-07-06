2dc2b409073de55a9cfacecee2c414dee9bead92
Sakdipat Ontoum <sakdipat3536@gmail.com>
2020-07-06 17:50:06 +0700

    [add] add payment screen

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,16 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
-const firebase = admin.firestore();
-const settings = {timestampeInSnapshots: true};
-firebase.settings(settings);
-
-const stripe = require("stripe")(functions.config().stripe.token);
-
-exports.addStripeSource = functions.firestore.document('cards/{userid}/tokens/{tokenid}')
- .onCreate(async (tokenSnap, context) => {
-     var customer;
-     const data = tokenSnap.after.data();
-     if(data === null) {
-         return null;
-     }
-     const token = data.tokenId;
-     const snapshot = await firestore.collection('cards').doc(context.params.userId).get();
-     const customerId = snapshot.data().custId;
-     const customerEmail = snapshot.data().email;
-
-     if(customerId === 'new') {
-         customer = await stripe.customers.create({
-             email: customerEmail,
-             source: token
-         });
-    
-         firestore.collection('cards').doc(context.params.userId).update({
-             customerId: customer.id
-         });
-     }
-     else {
-         customer = await stripe.customers.retrieve(customerId);
-     }
-
-     const customerSource = customer.sources.data[0];
-
-     return first.collection('cards').doc(context.params.userId)
-    .collection('sources').doc(customerSource.card.fingerprint).set(customerSource, {merge: true});
- })
+const firestore = admin.firestore();
+const settings = { timestampInSnapshots: true };
+firestore.settings(settings);
+const stripe = require("stripe")(
+  "sk_test_51GzJm8LlTCGQsQVIWZvsQFxBrxDC7giLpAH2vo0qxoRErvSplgxJ3FEGXRic5Jd2yCoFWBrR6HkAcVcs8IiQKvzp002cPp1mZN"); //TODO: ADD SECRET KEY
+exports.createPaymentIntent = functions.https.onCall((data, context) => {
+  return stripe.paymentIntents.create({
+    amount: data.amount,
+    currency: data.currency,
+    payment_method_types: ["card"],
+  });
+});
