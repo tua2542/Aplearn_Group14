743e2b0ebc4eaf615938612b78eb076021cd28cd
Sakdipat Ontoum <sakdipat3536@gmail.com>
2020-07-16 16:27:13 +0700

    [ADD/EDIT] make the text filed for typing the comment on vote page.

--- lib/src/Presenters/auth.dart
+++ lib/src/Presenters/auth.dart
@@ -1,6 +1,5 @@
 import 'package:aplearn_group14/src/Models/user.dart';
 import 'package:aplearn_group14/src/Presenters/database.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
     
--- lib/src/Views/vote/vote.dart
+++ lib/src/Views/vote/vote.dart
@@ -1,5 +1,8 @@
+import 'package:aplearn_group14/src/Presenters/comment.dart';
 import 'package:aplearn_group14/src/Views/vote/vote_widget/vote_widget.dart';
+import 'package:aplearn_group14/src/shared/loading.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class VoteApp extends StatefulWidget {
@@ -10,98 +13,91 @@ class VoteApp extends StatefulWidget {
 
 // _buildBody(context),
 class _VoteAppState extends State<VoteApp> {
+  final maxLines = 5;
+  FirebaseUser user;
+  String message = '';
+  String helper = '';
+  bool loading = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text('Voting Panel')),
-      body: Column(
-        children: <Widget>[
-          SizedBox(height: 20.0),
-          StreamBuilder<DocumentSnapshot>(
-              stream: Firestore.instance
-                  .collection('voteCourseTitle')
-                  .document('title')
-                  .snapshots(),
-              builder: (context, snapshot) {
-                return Container(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                  child: Center(
-                    child: RichText(
-                      text: new TextSpan(
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: snapshot.data['question'] + '\n',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(centerTitle: true, title: Text('Voting Panel')),
+            body: Column(
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                StreamBuilder<DocumentSnapshot>(
+                    stream: Firestore.instance
+                        .collection('voteCourseTitle')
+                        .document('title')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      return Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        child: Center(
+                          child: RichText(
+                            text: new TextSpan(
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: snapshot.data['question'] + '\n',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
+                        ),
+                      );
+                    }),
+                SizedBox(height: 10.0),
+                Flexible(
+                  child: VoteWidget(),
+                ),
+                Container(
+                  margin: EdgeInsets.all(12),
+                  height: maxLines * 24.0,
+                  child: TextField(
+                      maxLines: maxLines,
+                      decoration: InputDecoration(
+                        hintText: "Enter a message",
+                        fillColor: Colors.grey[300],
+                        filled: true,
                       ),
+                      onChanged: (val) {
+                        setState(() => message = val);
+                      }),
+                ),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                    color: Colors.pink[400],
+                    child: Text(
+                      'Submit',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ),
-                );
-              }),
-          Flexible(
-            child: VoteWidget(),
-          ),
-        ],
-      ),
-    );
+                    onPressed: () async {
+                      setState(() => loading = true);
+                      dynamic result =
+                          await CommentProvider().updateCommentData(message);
+                      if (result == null) {
+                        setState(() {
+                          loading = false;
+                          helper = 'Your message is sent';
+                        });
+                      }
+                    }
+                    ),
+                Text(
+                  helper,
+                  style: TextStyle(color: Colors.pink, fontSize: 14.0),
+                ),
+                SizedBox(height: 90.0),
+              ],
+            ),
+          );
   }
-
-  Widget _buildBody(BuildContext context) {
-    // TODO: get actual snapshot from Cloud Firestore
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('voteCourses').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-
-        return _buildList(context, snapshot.data.documents);
-      },
-    );
-  }
-
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final record = Record.fromSnapshot(data);
-
-    return Padding(
-      key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          onTap: () => record.reference.updateData({'votes': record.votes + 1}),
-        ),
-      ),
-    );
-  }
-}
-
-class Record {
-  final String name;
-  final int votes;
-  final DocumentReference reference;
-
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['courseName'] != null),
-        assert(map['votes'] != null),
-        name = map['courseName'],
-        votes = map['votes'];
-
-  Record.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
 }
