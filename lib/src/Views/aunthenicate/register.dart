a6fc3ef2aa9e515b7d5484faede4467431631e79
Sakdipat Ontoum <sakdipat3536@gmail.com>
2020-07-18 14:03:33 +0700

    [RESOLVE]

--- lib/src/Views/aunthenicate/register.dart
+++ lib/src/Views/aunthenicate/register.dart
@@ -1,5 +1,5 @@
 import 'package:aplearn_group14/src/Presenters/auth.dart';
-import 'package:aplearn_group14/src/shared/constants.dart';
+import 'package:aplearn_group14/src/Views/aunthenicate/sign_in.dart';
 import 'package:aplearn_group14/src/shared/loading.dart';
 import 'package:aplearn_group14/src/wrapper.dart';
 import 'package:flutter/material.dart';
@@ -18,7 +18,6 @@ class _RegisterState extends State<Register> {
   String _selectedDate = 'birth date';
   TextEditingController dateCtl = TextEditingController();
 
-
   // text field state
   String email = '';
   String password = '';
@@ -35,19 +34,18 @@ class _RegisterState extends State<Register> {
     final DateTime d = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
-      firstDate: DateTime(2015, 8),
+      firstDate: DateTime(1999, 1),
       lastDate: DateTime(2101),
-
+      
 
     );
-
+    
     if (d != null)
       setState(() {
         _selectedDate = new DateFormat.yMMMMd("en_US").format(d);
         dateCtl.text = _selectedDate.toString();
         birthdate = dateCtl.text;
         print(birthdate);
-
       });
   }
 
@@ -56,126 +54,183 @@ class _RegisterState extends State<Register> {
     return loading
         ? Loading()
         : Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign up to Aplearn'),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration:
-                textInputDecoration.copyWith(hintText: 'email'),
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
+//            backgroundColor: Colors.brown[100],
+//            appBar: AppBar(
+//              backgroundColor: Colors.brown[400],
+//              elevation: 0.0,
+//              title: Text('Sign up to Aplearn'),
+//            ),
+            body: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/background/bg1.jpg"),
+                  fit: BoxFit.cover,
+                ),
               ),
-              SizedBox(height: 10.0),
-              TextFormField(
-                decoration:
-                textInputDecoration.copyWith(hintText: 'password'),
-                obscureText: true,
-                validator: (val) => val.length < 6
-                    ? 'Enter a password 6+ chars long'
-                    : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 10.0),
-              TextFormField(
-                decoration:
-                textInputDecoration.copyWith(hintText: 'firstname'),
-                validator: (val) =>
-                val.isEmpty ? 'Enter an firstname' : null,
-                onChanged: (val) {
-                  setState(() => firstname = val);
-                },
-              ),
-              SizedBox(height: 10.0),
-              TextFormField(
-                decoration:
-                textInputDecoration.copyWith(hintText: 'lastname'),
-                validator: (val) =>
-                val.isEmpty ? 'Enter an lastname' : null,
-                onChanged: (val) {
-                  setState(() => lastname = val);
-                },
-              ),
-              SizedBox(height: 10.0),
-              TextFormField(
-                decoration:
-                textInputDecoration.copyWith(hintText: 'occupation'),
-                validator: (val) =>
-                val.isEmpty ? 'Enter an occupation' : null,
-                onChanged: (val) {
-                  setState(() => occupation = val);
-                },
-              ),
-              SizedBox(height: 10.0),
-              TextFormField(
-                  controller: dateCtl,
-                  decoration: textInputDecoration.copyWith(
-                      hintText: _selectedDate),
-                  validator: (val) =>
-                  val.isEmpty ? 'Enter an birthdate' : null,
-                  onChanged: (val) {
-                    setState(() => birthdate = val);
-                  },
-                  onTap: () {
-                    _selectDate(context);
-                    FocusScope.of(context).requestFocus(new FocusNode());
-                  }),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                  color: Colors.pink[400],
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 5.0),
+                      IconButton(
+                        padding: EdgeInsets.only(right: 500),
+                        icon: Icon(Icons.arrow_back_ios),
+                        color: Colors.black87,
+                        iconSize: 25.0,
+                        onPressed: () async {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignIn()));
+                        },
+                      ),
+                      Image.asset("assets/images/icon/acaIcon.png",
+                          width: 175, height: 175),
+                      Text(
+                        'Register',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.black87, fontSize: 22),
+                      ),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your email',
+                          labelText: 'Email',
+                        ),
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an email' : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your password',
+                          labelText: 'Password',
+                        ),
+                        obscureText: true,
+                        validator: (val) => val.length < 6
+                            ? 'Enter a password 6+ chars long'
+                            : null,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your firstname',
+                          labelText: 'Firstname',
+                        ),
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an firstname' : null,
+                        onChanged: (val) {
+                          setState(() => firstname = val);
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your lastname',
+                          labelText: 'Lastname',
+                        ),
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an lastname' : null,
+                        onChanged: (val) {
+                          setState(() => lastname = val);
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your occupation',
+                          labelText: 'Occupation',
+                        ),
+                        validator: (val) =>
+                            val.isEmpty ? 'Enter an occupation' : null,
+                        onChanged: (val) {
+                          setState(() => occupation = val);
+                        },
+                      ),
+                      TextFormField(
+                          controller: dateCtl,
+                          decoration: const InputDecoration(
+                            hintText: 'Select your birthdate',
+                            labelText: 'Birthdate',
+                          ),
+                          validator: (val) =>
+                              val.isEmpty ? 'Enter an birthdate' : null,
+                          onChanged: (val) {
+                            setState(() => birthdate = val);
+                          },
+                          onTap: () {
+                            _selectDate(context);
+                            FocusScope.of(context)
+                                .requestFocus(new FocusNode());
+                          }),
+                      SizedBox(height: 20.0),
+                      Container(
+                        height: 40,
+                        width: 275,
+                        child: RaisedButton(
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() => loading = true);
+                              dynamic result =
+                                  await _auth.registerWithEmailAndPassword(
+                                      email,
+                                      password,
+                                      firstname,
+                                      lastname,
+                                      occupation,
+                                      role,
+                                      avatar,
+                                      birthdate);
+                              if (result == null) {
+                                setState(() {
+                                  loading = false;
+                                  error = 'Please supply a valid email';
+                                });
+                              } else {
+                                Navigator.pushAndRemoveUntil(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Wrapper()),
+                                    (route) => false);
+                              }
+                            }
+                          },
+                          padding: EdgeInsets.all(0.0),
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [Color(0xff374ABE), Color(0xff64B6FF)],
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                              ),
+                            ),
+                            child: Container(
+                              constraints: BoxConstraints(
+                                  maxWidth: 300.0, minHeight: 50.0),
+                              alignment: Alignment.center,
+                              child: Text(
+                                "Register",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12.0),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      )
+                    ],
                   ),
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      setState(() => loading = true);
-                      dynamic result =
-                      await _auth.registerWithEmailAndPassword(
-                          email,
-                          password,
-                          firstname,
-                          lastname,
-                          occupation,
-                          role,
-                          avatar,
-                          birthdate);
-                      if (result == null) {
-                        setState(() {
-                          loading = false;
-                          error = 'Please supply a valid email';
-                        });
-                      } else {
-                        Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Wrapper()),
-                                (route) => false);
-                      }
-                    }
-                  }),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+                ),
+              ),
+            ),
+          );
   }
 }
